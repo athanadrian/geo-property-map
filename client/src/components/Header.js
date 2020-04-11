a37498836050a30b2b89ebdce8805bb2d66d373c
athanadrian <athanadrian@gmail.com>
2020-04-11 08:51:06 +0300

    fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
               color="inherit"
               noWrap
             >
-              GeoPins
+              GeoProperty
             </Typography>
           </div>
 
